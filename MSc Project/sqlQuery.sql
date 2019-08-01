3c2e323ccd5101e87ab06d340ce91d03f1bedc2c
Kashif Ahmed <kashif-999@outlook.com>
2019-08-01 19:38:02 +0100

    sql

--- MSc Project/sqlQuery.sql
+++ MSc Project/sqlQuery.sql
@@ -1,14 +1,17 @@
 USE msc;
-DROP TABLE IF EXISTS msc.user_hobby;
+
+DROP TABLE IF EXISTS msc.user_hobby; 
 DROP TABLE IF EXISTS msc.hobby;
-DROP TABLE IF EXISTS msc.user_detail;
-DROP TABLE IF EXISTS msc.user_authentication;
+DROP TABLE IF EXISTS msc.user_detail; 
+DROP TABLE IF EXISTS msc.user_authentication; 
+
 CREATE TABLE IF NOT EXISTS user_authentication (
     id INT AUTO_INCREMENT,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
+
 CREATE TABLE IF NOT EXISTS user_detail (
     id INT AUTO_INCREMENT,
     first_name VARCHAR(255) NOT NULL,
@@ -17,15 +20,16 @@ CREATE TABLE IF NOT EXISTS user_detail (
     location VARCHAR(255) NOT NULL,
     _status VARCHAR(20) NOT NULL,
     username INT NOT NULL,
-				profile_img VARCHAR(100) NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (username) REFERENCES user_authentication(id)
 );
+
 CREATE TABLE IF NOT EXISTS hobby (
     id INT AUTO_INCREMENT,
     hobby VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
+
 CREATE TABLE IF NOT EXISTS user_hobby (
     id INT AUTO_INCREMENT,
     hobby_fk INT NOT NULL,
@@ -34,20 +38,23 @@ CREATE TABLE IF NOT EXISTS user_hobby (
     FOREIGN KEY (hobby_fk) REFERENCES hobby(id),
     FOREIGN KEY (user_fk) REFERENCES user_detail(id)
 );
+
+/* user_authentication */
 INSERT INTO user_authentication (username, password) 
 VALUES ('username', 'password');
 
-INSERT INTO user_detail (first_name, last_name, gender, location, _status, username, profile_img) 
-VALUES ('john', 'wick', 'm', 'Bolton', 'online', 1, 'https://www.aspentrading.com/wp-content/uploads/dave-floyd-head-shot-12.19.2016-300x300.png');
+/* user_detail */
+INSERT INTO user_detail (first_name, last_name, gender, location, _status, username) 
+VALUES ('john', 'wick', 'm', 'Bolton', 'online', 1);
 
-INSERT INTO hobby (hobby) 
-VALUES ('swimming');
+/* hobby */
 INSERT INTO hobby (hobby) 
 VALUES ('reading');
+INSERT INTO hobby (hobby) 
+VALUES ('swimming');
 
+/* user_hobby */
 INSERT INTO user_hobby (hobby_fk, user_fk) 
 VALUES (1, 1);
-INSERT INTO user_hobby (hobby_fk, user_fk) 
-VALUES (2, 1);
 INSERT INTO user_hobby (hobby_fk, user_fk) 
 VALUES (2, 1);
